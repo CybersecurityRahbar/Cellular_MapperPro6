9e5afa6f7eabdd43106cdaad3835175a0777cfb9
CybersecurityRahbar <alghfryloberalghwly@gmail.com>
2026-06-27 01:57:16 +0300

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,48 +1,16 @@
+// Top-level build file
 plugins {
-    id("com.android.application") apply false
-    id("com.android.library") apply false
-    kotlin("android") apply false
-    alias(libs.plugins.compose.compiler) apply false
-    alias(libs.plugins.detekt)
-    alias(libs.plugins.nexus.publish)
-    cleanup
-    base
+    // not needed for root
 }
 
 allprojects {
-    val GROUP: String by project
-    val VERSION: String by project
-    val USE_SNAPSHOT: String? by project
-    group = GROUP
-    version = if (USE_SNAPSHOT.toBoolean()) "$VERSION-SNAPSHOT" else VERSION
-}
-
-val detektFormatting = libs.detekt.formatting
-
-subprojects {
-    apply {
-        plugin("io.gitlab.arturbosch.detekt")
-    }
-
-    detekt {
-        config.from(rootProject.files("config/detekt/detekt.yml"))
-    }
-
-    dependencies {
-        detektPlugins(detektFormatting)
-    }
-}
-
-val NEXUS_USERNAME: String? by project
-val NEXUS_PASSWORD: String? by project
-
-nexusPublishing {
     repositories {
-        sonatype {
-            nexusUrl.set(uri("https://ossrh-staging-api.central.sonatype.com/service/local/"))
-            snapshotRepositoryUrl.set(uri("https://central.sonatype.com/repository/maven-snapshots/"))
-            username.set(NEXUS_USERNAME)
-            password.set(NEXUS_PASSWORD)
-        }
+        google()
+        mavenCentral()
+        maven { url = uri("https://jitpack.io") } // لـ MPAndroidChart
     }
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
 }
