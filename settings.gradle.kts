8f5df69de62145b1fab3c452c8a76a26eeca548f
CybersecurityRahbar <alghfryloberalghwly@gmail.com>
2026-06-27 03:18:13 +0300

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -5,14 +5,17 @@ pluginManagement {
         gradlePluginPortal()
         maven { url = uri("https://jitpack.io") }
     }
+
     plugins {
         id("com.android.application") version "8.5.0"
         id("org.jetbrains.kotlin.android") version "2.0.0"
-        id("kotlin-kapt") version "2.0.0"
+        id("org.jetbrains.kotlin.kapt") version "2.0.0"
     }
 }
 
 dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+
     repositories {
         google()
         mavenCentral()
